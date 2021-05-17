d0b8cfe4039ae992d94d1abd15cc43045af6fe8d
radicm <milosr@origo.is>
2021-05-17 17:45:12 +0200

    Add api routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,22 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  namespace :api, :defaults => { :format => 'json' } do
+    namespace :v1 do
+      post "login" => "authentications#login"
+      post "logout" => "authentications#logout"
+
+      namespace :users do
+        get "notifications"
+      end
+
+      resources :users, only: [] do
+        get :portfolios
+      end
+
+      resources :notifications, only: [:index, :create] do
+        put :send_notification
+      end
+    end
+  end
 end
